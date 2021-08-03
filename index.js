e7c33691e08c71b58b9caa53a03914ddd96c4d20
smarapane <sidathmarapane@yahoo.com>
2021-08-03 12:17:22 -0400

    Handle OPTIONS requests

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ let apiRoutes = require("./api-routes");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-// Connect to Mongoose and set connection variable
+// Handle OPTIONS request 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.sendStatus(200);
+    }
+    else {
+    //move on
+      next();
+    }
+});// Connect to Mongoose and set connection variable
 mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true});
 var db = mongoose.connection;
 
